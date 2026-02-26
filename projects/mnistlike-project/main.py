6dc0dba6d89ce020a949a80e4448337c51879fb7
Mercy-Nzambi <mercymbuthi5@gmail.com>
2026-02-26 16:57:13 +0300

    Completed both my first project and it's write-up

--- projects/mnistlike-project/main.py
+++ projects/mnistlike-project/main.py
@@ -12,15 +12,15 @@ X = digits.data #this contains the input images
 y = digits.target #this contains the digit values
 
 #viewing the shape of the data
-print(X.shape)
-print(y.shape)
+print(f"The shape of the samples and features is: {X.shape}")
+print(f"The shape of the labels: {y.shape}")
 
 #splitting the data
 #random state is for reproducibility, shuffle is 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
 #creating, training and testing the model 
-model = MLPClassifier(hidden_layer_sizes=100, activation='relu', solver='adam', max_iter=300)
+model = MLPClassifier(hidden_layer_sizes=1000, activation='relu', solver='adam', max_iter=300)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
@@ -31,8 +31,13 @@ print(f"The accuracy of the model: {accuracy:.4f}")
 #visualizing the data
 #working with images so use imshow
 #using a single test sample and reshaping it to its original form
-X_test_sample = X_test[0]
-X_test_reshape = X_test_sample.reshape(-1, 1)
+X_test_sample = X_test[60]
+print(f"The shape of the sample is: {X_test_sample.shape}")
+X_test_reshape = X_test_sample.reshape(8, 8)
 plt.imshow(X_test_reshape, cmap='gray')
 plt.axis("off")
 plt.show()
+y_test_sample = y_test[60]
+y_pred_sample = y_pred[60]
+print(f"The actual number displayed is: {y_test_sample}")
+print(f"The number the model predicted is: {y_pred_sample}")
